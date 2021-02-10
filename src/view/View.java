c7f2e83c4a970fd95b7cdef9068ac7c1498c9b10
alejandra365 <69652051+alejandra365@users.noreply.github.com>
2021-02-09 23:05:40 -0500

    Add files via upload

--- src/view/View.java
+++ src/view/View.java
@@ -19,8 +19,10 @@ public class View
 			System.out.println("3. Buscar String");
 			System.out.println("4. Eliminar String");
 			System.out.println("5. Imprimir el Arreglo");
-			System.out.println("6. Exit");
+			System.out.println("6. Invertir lista");
+			System.out.println("7. Exit");
 			System.out.println("Dar el numero de opcion a resolver, luego oprimir tecla Return: (e.g., 1):");
+
 		}
 
 		public void printMessage(String mensaje) {
@@ -31,5 +33,7 @@ public class View
 		public void printModelo(Modelo modelo)
 		{
 			// TODO implementar
+		
+			System.out.println(modelo);
 		}
 }
